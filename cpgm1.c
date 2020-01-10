09dcb830b754dcec223d8277099b60dc4b69a397
naveenkannan2305 <31180520+naveenkannan2305@users.noreply.github.com>
2020-01-10 04:39:04 -0500

    Update cpgm1.c

--- cpgm1.c
+++ cpgm1.c
@@ -9,10 +9,7 @@ void swap(int *xp, int *yp)
 int main() 
 { 
     int x, y; 
-    printf("Enter Value of x "); 
-    scanf("%d", &x); 
-    printf("\nEnter Value of y "); 
-    scanf("%d", &y);
+    x=10,y=20;
     swap(&x,&y);
     printf("\nAfter Swapping: x = %d, y = %d", x, y); 
     return 0; 
